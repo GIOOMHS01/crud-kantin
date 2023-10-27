3e4c18ae9db9a1fb6a49d3b8ebd863ea1be9bd12
GIOOMHS01 <gio736066@gmail.com>
2023-10-27 11:55:20 +0700

    sudah bisa terupdate dan di edit

--- edit.php
+++ edit.php
@@ -2,41 +2,39 @@
 // include database connection file
 include_once("config.php");
 
-// Check if form is submitted for user update, then redirect to homepage after update
+// Check if form is submitted for menu update, then redirect to homepage after update
 if (isset($_POST['update'])) {
+    $nama = $_POST['nama'];
     $id = $_POST['id'];
-
-    $id_menu = $_POST['id_menu'];
     $jenis = $_POST['jenis'];
     $harga = $_POST['harga'];
-    $id_penjual = $_POST['id_penjual'];
+    $id_penjual = $_POST['id_penjual']; // Update the id_penjual
 
-    // update user data
-    $result = mysqli_query($mysqli, "UPDATE menu SET id_menu='$id_menu',jenis='$jenis',harga='$harga',id_penjual='$id_penjual' WHERE id_menu=$id");
+    // Update menu data
+    $result = mysqli_query($mysqli, "UPDATE menu SET nama='$nama', jenis='$jenis', harga='$harga', id_penjual='$id_penjual' WHERE id_menu=$id");
 
-    // Redirect to homepage to display updated user in list
+    // Redirect to homepage to display updated menu in list
     header("Location: menu.php");
 }
-?>
-<?php
-// Display selected user data based on id
-// Getting id from url
+
+// Display selected menu data based on id
+// Getting id from URL
 $id = $_GET['id'];
 
-// Fetech user data based on id
+// Fetch menu data based on id
 $result = mysqli_query($mysqli, "SELECT * FROM menu WHERE id_menu=$id");
 
-while ($user_data = mysqli_fetch_array($result)) {
-    $id_menu = $user_data['id_menu'];
-    $jenis = $user_data['jenis'];
-    $harga = $user_data['harga'];
-    $id_penjual = $user_data['id_penjual'];
+while ($menu_data = mysqli_fetch_array($result)) {
+    $nama = $menu_data['nama'];
+    $jenis = $menu_data['jenis'];
+    $harga = $menu_data['harga'];
+    $id_penjual = $menu_data['id_penjual'];
 }
 ?>
 <html>
 
 <head>
-    <title>Edit User Data</title>
+    <title>Edit Menu Data</title>
 </head>
 
 <body>
@@ -46,26 +44,26 @@ while ($user_data = mysqli_fetch_array($result)) {
     <form name="update_menu" method="post" action="edit.php">
         <table border="1">
             <tr>
-                <td>id_menu</td>
-                <td><input type="text" name="id_menu" value=<?php echo $id_menu; ?>></td>
+                <td>Nama</td>
+                <td><input type="text" name="nama" value="<?php echo $nama; ?>"></td>
             </tr>
             <tr>
-                <td>jenis</td>
+                <td>Jenis</td>
                 <td>
                     <select name="jenis" id="">
-                        <option value="makanan">Makanan</option>
-                        <option value="minuman">Minuman</option>
+                        <option value="makanan" <?php if ($jenis == "makanan") echo "selected"; ?>>Makanan</option>
+                        <option value="minuman" <?php if ($jenis == "minuman") echo "selected"; ?>>Minuman</option>
                     </select>
                 </td>
             </tr>
             <tr>
-                <td>harga</td>
-                <td><input type="number" name="harga" value=<?php echo $harga; ?>></td>
+                <td>Harga</td>
+                <td><input type="number" name="harga" value="<?php echo $harga; ?>"></td>
             </tr>
             <tr>
-                <td>penjual</td>
+                <td>Penjual</td>
                 <td>
-                    <select name="idp">
+                    <select name="id_penjual">
                         <?php
                         include_once("config.php");
                         $penjual = mysqli_query($mysqli, "SELECT * FROM penjual ORDER BY idp DESC");
@@ -75,11 +73,11 @@ while ($user_data = mysqli_fetch_array($result)) {
                             echo '<option value="' . $data['idp'] . '"' . $selected . '>' . $data['nama_penjual'] . '</option>';
                         }
                         ?>
+                    </select>
                 </td>
             </tr>
             <tr>
-            <tr>
-                <td><input type="hidden" name="id" value=<?php echo $_GET['id']; ?>></td>
+                <td><input type="hidden" name="id" value="<?php echo $_GET['id']; ?>"></td>
                 <td><input type="submit" name="update" value="Update"></td>
             </tr>
         </table>
